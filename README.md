2e00300285fa98be6c85b24a45774fd25891a3c5
M Hamza Kamran <hkwaheedi@gmail.com>
2025-02-27 23:42:17 +0500

    First data

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# facebook-interval
-javascript
+# CurrentTime
+Display Current Time using JavaScript
+![current time](https://github.com/Laman-dev/CurrentTime/assets/97945337/3ee4fd93-b5a3-45e6-aab8-b0cba6d56d02)
---
+++ time.js
@@ -0,0 +1,18 @@
+
+// setInterval(()=>{
+//     let mydate = new Date()
+//     let time = mydate.toLocaleTimeString()
+//     document.getElementById("time").innerHTML = time
+// },1000)
+
+
+
+setInterval(() =>{
+    let mydate= new Date()
+  
+    let time = mydate.toLocaleTimeString()
+    document.getElementById("time").innerHTML = time
+window.open("https://www.facebook.com","_blank")
+    
+
+} ,1000  )
